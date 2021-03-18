724e39c0f63c18f303696268db02d185544ca6b6
jubi-in <thetruthrunswild@naver.com>
2021-03-18 16:38:46 +0900

    #5.4

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,20 +1,27 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Redirect, Switch} from "react-router-dom";
+import { BrowserRouter as Router, Route, Redirect, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import TV from "Routes/TV";
 import Search from "Routes/Search";
+import Detail from "Routes/Detail";
 import Header from "Components/Header";
 
-export default () => (
-    <Router>
-        <>
-        <Header />
-        <Switch>
-            <Route path="/" component={Home} />
-            <Route path="/tv" component={TV} />
-            <Route path="/search" component={Search} />
-            <Redirect from="*" to="/" />
-        </Switch>
-        </>
-    </Router>
-);
+const router = () => {
+    return (
+        <Router>
+            <>
+                <Header />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/tv" component={TV} />
+                    <Route path="/search" component={Search} />
+                    <Route path="/movie/:id" component={Detail} />
+                    <Route path="/show/:id" component={Detail} />
+                    <Redirect from="*" to="/" />
+                </Switch>
+            </>
+        </Router>
+    );
+}
+
+export default router;
--- src/api.js
+++ src/api.js
@@ -1,30 +1,31 @@
-import axios from axios;
+import axios from "axios";
 
-const api = axios.create( {
-    basURL: "https://api.themoviedb.org/3/",
+const api = axios.create({
+    baseURL: "https://api.themoviedb.org/3/",
     params: {
         api_key: "92de913c9e9a0de95f98a3fd8ed672c8",
         language: "en-US"
     }
 });
 
-export const tvApi = {
+export const moviesApi = {
     nowPlaying: () => api.get("movie/now_playing"),
     upcoming: () => api.get("movie/upcoming"),
     popular: () => api.get("movie/popular"),
-    moiveDeail: id => api.get(`movie/${id}`, {
+    moiveDetail: id => api.get(`movie/${id}`, {
         params: {
             append_to_response: "videos"
         }
     }),
-    search: term => api.get("search/movie", {
-        params: {
-            query: term
-        }
-    })
+    search: term =>
+        api.get("search/movie", {
+            params: {
+                query: term
+            }
+        })
 }
 
-export const movieApi = {
+export const tvApi = {
     topRated: () => api.get("tv/top_rated"),
     popular: () => api.get("tv/popular"),
     airingToday: () => api.get("tv/airing_today"),
@@ -33,9 +34,10 @@ export const movieApi = {
             append_to_response: "videos"
         }
     }),
-    search: term => api.get("search/tv", {
-        params: {
-            query: term
-        }
-    })
+    search: term =>
+        api.get("search/tv", {
+            params: {
+                query: term
+            }
+        })
 }
