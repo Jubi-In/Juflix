f4c5eefe0b13b62f7d7026f82c0da42d9231adb7
jubi-in <thetruthrunswild@naver.com>
2021-03-23 17:12:15 +0900

    #6.10 finish

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from "react-helmet";
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from "Components/Message";
@@ -11,58 +12,63 @@ const Container = styled.div`
 `;
 
 const HomePresenter = ({ nowPlaying, popular, upcoming, loading, error }) =>
-    loading ? (
-        <Loader />
-    ) : (
-        <Container>
-            {nowPlaying && nowPlaying.length > 0 && (
-                <Section title="Now Playing">
-                    {nowPlaying.map(movie => (
-                        <Poster 
-                            key={movie.id}
-                            id={movie.id}
-                            imageUrl={movie.poster_path}
-                            title={movie.original_title}
-                            rating={movie.vote_average}
-                            year={movie.release_date && movie.release_date.substring(0, 4)}
-                            isMovie={true}
-                        />
-                    ))}
-                </Section>
-            )}
-            {upcoming && upcoming.length > 0 && (
-                <Section title="Upcoming Movies">
-                    {upcoming.map(movie => (
-                        <Poster 
-                        key={movie.id}
-                        id={movie.id}
-                        imageUrl={movie.poster_path}
-                        title={movie.original_title}
-                        rating={movie.vote_average}
-                        year={movie.release_date && movie.release_date.substring(0, 4)}
-                        isMovie={true}
-                    />
-                    ))}
-                </Section>
-            )}
-            {popular && popular.length > 0 && (
-                <Section title="Popular Movies">
-                    {popular.map(movie => (
-                        <Poster 
-                        key={movie.id}
-                        id={movie.id}
-                        imageUrl={movie.poster_path}
-                        title={movie.original_title}
-                        rating={movie.vote_average}
-                        year={movie.release_date && movie.release_date.substring(0, 4)}
-                        isMovie={true}
-                    />
-                    ))}
-                </Section>
-            )}
-            {error && <Message color="white" text={error} />}
-        </Container>
-    );
+    <>
+        <Helmet>
+            <title>Movies | Juflix</title>
+        </Helmet>
+        {loading ? (
+            <Loader />
+        ) : (
+            <Container>
+                {nowPlaying && nowPlaying.length > 0 && (
+                    <Section title="Now Playing">
+                        {nowPlaying.map(movie => (
+                            <Poster
+                                key={movie.id}
+                                id={movie.id}
+                                imageUrl={movie.poster_path}
+                                title={movie.original_title}
+                                rating={movie.vote_average}
+                                year={movie.release_date && movie.release_date.substring(0, 4)}
+                                isMovie={true}
+                            />
+                        ))}
+                    </Section>
+                )}
+                {upcoming && upcoming.length > 0 && (
+                    <Section title="Upcoming Movies">
+                        {upcoming.map(movie => (
+                            <Poster
+                                key={movie.id}
+                                id={movie.id}
+                                imageUrl={movie.poster_path}
+                                title={movie.original_title}
+                                rating={movie.vote_average}
+                                year={movie.release_date && movie.release_date.substring(0, 4)}
+                                isMovie={true}
+                            />
+                        ))}
+                    </Section>
+                )}
+                {popular && popular.length > 0 && (
+                    <Section title="Popular Movies">
+                        {popular.map(movie => (
+                            <Poster
+                                key={movie.id}
+                                id={movie.id}
+                                imageUrl={movie.poster_path}
+                                title={movie.original_title}
+                                rating={movie.vote_average}
+                                year={movie.release_date && movie.release_date.substring(0, 4)}
+                                isMovie={true}
+                            />
+                        ))}
+                    </Section>
+                )}
+                {error && <Message color="white" text={error} />}
+            </Container>
+        )}
+    </>;
 
 HomePresenter.propTypes = {
     nowPlaying: PropTypes.array,
